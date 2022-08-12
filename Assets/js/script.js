3a0ec4b079c76e469b34d852b1b0a23682d2b06f
Justin Klaver <Klaverts@yahoo.com>
2022-08-12 17:25:15 -0400

    added ability to save each block to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,13 @@
+
+$( ".saveBtn" ).click(function() {
+    PlannerText = $(this).siblings(".description").val();
+    TimeBlock = $(this).parent().attr("id");
+    console.log(TimeBlock);
+    console.log(PlannerText);
+
+    localStorage.setItem(TimeBlock,PlannerText);
+  });
+
 function TimeCompare() {
     //Gets the current hour number 0-23
     LocalTime = moment().hours();
@@ -6,7 +16,6 @@ function TimeCompare() {
     $(".time-block").each(function () {
         //sets up planner time by removing "hour" from the Id leaving the number and transforming from a string to an int. 
         PlanTime = parseInt($(this).attr("id").split("hour")[1]);
-
 
         //Comparing planner time with local time and applying appropriate background classes
         if (PlanTime > LocalTime) {
