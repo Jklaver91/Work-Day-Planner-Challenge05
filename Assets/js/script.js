f51e46fb84d0dfaff939b361cd2fc2763ef7a9c0
Justin Klaver <Klaverts@yahoo.com>
2022-08-12 17:32:49 -0400

    added comments for saving and formatting

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,18 @@
-
+//on clicking save button grabs sibling class of sveBtn (description) and saves both the parent class (hour__) and the text to be recalled later.
 $( ".saveBtn" ).click(function() {
     PlannerText = $(this).siblings(".description").val();
     TimeBlock = $(this).parent().attr("id");
-    console.log(TimeBlock);
-    console.log(PlannerText);
-
     localStorage.setItem(TimeBlock,PlannerText);
   });
 
 function TimeCompare() {
-    //Gets the current hour number 0-23
-    LocalTime = moment().hours();
 
-    //time-block loop
     $(".time-block").each(function () {
         //sets up planner time by removing "hour" from the Id leaving the number and transforming from a string to an int. 
         PlanTime = parseInt($(this).attr("id").split("hour")[1]);
-
+          //Gets the current hour number 0-23
+        LocalTime = moment().hours();
+        
         //Comparing planner time with local time and applying appropriate background classes
         if (PlanTime > LocalTime) {
             $(this).removeClass("past");
